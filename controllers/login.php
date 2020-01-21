a09c54f49ed64411df8d26e3e7e852618b2d4600
password4gitlab <vidya.sheval24@gmail.com>
2020-01-21 13:23:41 +0530

    folder added

---
+++ controllers/login.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "hello";
+/*
+$x = 'http://url.com/search/?location=london&page_number=1';
+
+$parsed = parse_url($x);
+$query = $parsed['query'];
+
+parse_str($query, $params);
+
+unset($params['page_number']);
+$string = http_build_query($params);
+var_dump($string);*/
+?>
